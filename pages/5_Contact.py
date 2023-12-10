07dc08e17b0583fefe86450ffc687ec4581810cf
Sandro Radovanović <sandro.radovanovic@hotmail.com>
2023-12-10 18:54:13 +0100

    Update 5_Contact.py

---
+++ pages/5_Contact.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.set_page_config(layout='wide', page_title = 'Contact')
+st.title("Contact")
+
+st.markdown(
+    '''
+    If you are interested in our work, please visit our [website](https://mindproject.ac.rs/). The information related to the Balkan Peace Index can be found [here](https://bpi.mindproject.ac.rs/). However, there are other projects such as [Status of Kosovo recognition & derecognition](https://statusofkosovo.info/) and [Regional Security Initiatives](https://rsi.mindproject.ac.rs/).
+
+    For questions related to the Balkan Peace Index and the process of validation please contact Principal Investigator [Nemanja Džuverović](mailto:nemanja.dzuverovic@fpn.bg.ac.rs).
+
+    Quesstions related to the DEX methodology and the website please contact [Sandro Radovanović](mailto:sandro.radovanovic@fon.bg.ac.rs). 
+    '''
+)
